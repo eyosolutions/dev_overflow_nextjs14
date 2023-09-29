4a706958fe463615aeefedc6e49b6d7170bc6137
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-09-29 21:08:24 +0000

    added markup text to global layout and page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="h1-bold">NextJS 13 we&apos;re coming</h1>
+      <h2 className="h2-bold">NextJS 13 we&apos;re coming</h2>
+      <h3 className="h3-bold">NextJS 13 we&apos;re coming</h3>
     </div>
   );
 };
