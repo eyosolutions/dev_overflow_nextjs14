8599a8ad2cddba2609c92776a912786f0cbeb6d6
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-09-30 21:34:12 +0000

    add assets and layout

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { UserButton } from '@clerk/nextjs';
 const Home = () => {
   return (
     <div>
+      <h1 className='h1-bold'>This is a global layout text</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,28 @@ import React from 'react';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 
-import { Inter } from 'next/font/google';
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 
 export const metadata: Metadata = {
-  title: 'Dev-Overflow App',
-  description: 'Like stack overflow and creeated with Nextjs and ClerkAuth',
+  title: 'DevOverflow',
+  description: 'A community-driven platform for asking and answering programming questions. Get help, share knowledege, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures and more.',
+  icons: {
+    icon: '/public/assets/images/site-logo.svg'
+  },
 };
 
 export default function RootLayout({
@@ -18,9 +32,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover: text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
