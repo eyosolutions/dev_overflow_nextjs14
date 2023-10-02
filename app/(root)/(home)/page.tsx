085e17e0e70725e4c98841bb65f78dd1c21bf9ee
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-10-02 01:33:40 +0000

    implement theme provider

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from '@clerk/nextjs';
 const Home = () => {
   return (
     <div>
-      <h1 className='h1-bold'>This is a global layout text</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from '@/context/ThemeProvider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -32,19 +33,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: 'primary-gradient',
-          footerActionLink: 'primary-text-gradient hover: text-primary-500'
-        }
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <ClerkProvider
+        appearance={{
+          elements: {
+            formButtonPrimary: 'primary-gradient',
+            footerActionLink: 'primary-text-gradient hover: text-primary-500'
+          }
+        }}
+      >
+        <ThemeProvider>
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+        </ThemeProvider>
+      </ClerkProvider>
+      </body>
+    </html>
   );
 };
