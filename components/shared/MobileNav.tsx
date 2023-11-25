108774707dcdf23403720e9090c6945fdc756cc4
eyosolutions <emmanuel.y.odoom@gmail.com>
2023-11-25 07:29:42 +0000

    hiding profile link in mobileNav when logged out

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/sheet"
 
 import React from 'react'
-import { SignedOut, useAuth } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useAuth } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
@@ -17,22 +17,21 @@ import { usePathname } from "next/navigation";
 const NavContent = () => {
   const pathname = usePathname();
   const { userId } = useAuth();
+
+  const sidebarLinkWithProfile = userId
+  ? sidebarLinks.map((item) =>
+      item.route === '/profile' ? { ...item, route: `${item.route}/${userId}` } : item
+    )
+  : [];
+
+  const sidebarLinkWithoutProfile = (!userId ? sidebarLinks : [])
+  .filter((item) => item?.route !== '/profile');
   
   return (
     <section className="flex h-full flex-col gap-6 pt-16">
-      {sidebarLinks
-        .map((item: any) => {
-          if (item.route === '/profile') {
-            if (userId) {
-              item.route = `${item.route}/${userId}`;
-              return item;
-            }
-            return null;
-          }
-          return item;
-        })
-        .filter((item) => item)
-        .map((item) => {
+      {/* rendering the sidebarLinks when signed out */}
+      <SignedOut>
+        {sidebarLinkWithoutProfile.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
           return (
@@ -52,7 +51,34 @@ const NavContent = () => {
               </Link>
             </SheetClose>
           );
-      })}
+        })}
+      </SignedOut>
+
+      {/* rendering the sidebarLinks when signed in */}
+      <SignedIn>
+        {sidebarLinkWithProfile.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+          return (
+            <SheetClose asChild key={item.route}>
+              <Link href={item.route} className={`${isActive 
+                ? 'primary-gradient rounded-lg text-light-900'
+                : 'text-dark300_light900'} flex items-center justify-start gap-4 bg-transparent p-4`}
+              >
+                <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${!isActive && 'invert-colors'}`}
+                />
+                <p className={`${isActive ? 'base-bold' : 'base-medium'}`}>{item.label}</p>
+              </Link>
+            </SheetClose>
+          );
+        })}
+      </SignedIn>
+
     </section>
   )
 }
